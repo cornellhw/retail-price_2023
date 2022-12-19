d48aa5501a24e461c795cd3df5b92da8672e59b5
wendan song <songwendan3@163.com>
2022-12-19 11:34:47 -0800

    description:new updates

--- exp_2/models.py
+++ exp_2/models.py
@@ -375,8 +375,8 @@ class Player(BasePlayer):
         return
 
     def set_payoff2(self):
-        self.sell = 28 * int(max(1-(self.R - self.session.config['l2']) / (self.session.config['u2'] - self.session.config['l2']), 0))
-        self.earn = self.R * self.sell - self.W
+        self.sell = 28 * (max(1-(self.R - self.session.config['l2']) / (self.session.config['u2'] - self.session.config['l2']), 0))
+        self.earn = int(self.R * self.sell - self.W)
         self.market_coverage = self.sell/28
         self.profit_bonus = self.session.config['a2'] * (self.R * self.sell - self.W)
         if self.lockin2 != 'lockin':
