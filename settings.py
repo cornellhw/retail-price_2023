ff54bbbe119e29159b4b9f61afa4565e12786dbd
wendan song <songwendan3@163.com>
2023-02-04 12:03:43 -0500

    description:new updates

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ from os import environ
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+    real_world_currency_per_point=1.00,participation_fee=0.00, doc=""
 )
 PARTICIPANT_FIELDS = ['consent', 'payoff_cem', 'payoff_trust']
 
